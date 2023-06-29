f476795b051b493285b67837dcf151585933efe4
seeyoo <lukdown10@gmail.com>
2023-06-29 11:39:27 +0900

    ,.,.

--- code/mysite/polls/urls.py
+++ code/mysite/polls/urls.py
@@ -8,4 +8,14 @@ urlpatterns = [
     # name="경로 이름"
     # http://localhost:8000/polls  -->  index()
     path("", views.index, name="index"),
+    path("<int:question_id>/", views.detail, name="detail"),            # /polls/10
+    path("<int:question_id>/vote", views.vote, name="vote"),            # /polls/10/vote
+    path("<int:question_id>/results", views.results, name="results"),   # /polls/10/results
+    
 ]
+
+'''
+    path("<int:question_id>/", ... )
+    - polls/10
+    : 10 이라는 데이터를 요청변수 int타입의 question_id 로 연결해준다.
+'''
--- code/mysite/polls/views.py
+++ code/mysite/polls/views.py
@@ -1,6 +1,33 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from polls.models import Question, Choice
 
 # Create your views here.
+# 설문 목록 뷰
 def index(request):
-    return HttpResponse('Hello')
+    # return HttpResponse('Hello')
+    # 설문 목록을 조회
+    question_list = Question.objects.all()  # Question 테이블의 전체목록
+    context = {'question_list' : question_list}
+    return render(request, 'polls/index.html', context)
+
+
+# 설문 뷰
+def detail(request, question_id):
+    # question_id 로 데이터 조회
+    question = get_object_or_404(Question, pk=question_id)
+    
+    # 응답할 템플릿을 지정 (detail.html)
+    return render(request, 'polls/detail.html', {'question' : question})
+    # render()
+    # : 요청객체(request)를 지정해주고, 응답할 화면을 지정
+    #  HttpResponse 객체를 반환
+    #   - 클라이언트에게 응답할 내용
+    #   - 화면을 렌더링한 결과 포함
+    
+    
+    
+    
+    
+    
