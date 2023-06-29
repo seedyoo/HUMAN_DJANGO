5d728bd6d8f43365fef32d96c44ad17235d00fbe
seeyoo <lukdown10@gmail.com>
2023-06-29 13:58:39 +0900

    ,.

--- code/mysite/polls/views.py
+++ code/mysite/polls/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-from django.core.urlresolvers import reverse
+# from django.core.urlresolvers import reverse
+from django.urls import reverse
 
 from polls.models import Question, Choice
 
