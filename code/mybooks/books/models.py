ab336b0d7f0044471dff045e85176a36821dad53
seeyoo <lukdown10@gmail.com>
2023-07-03 14:37:05 +0900

    ,.,

---
+++ code/mybooks/books/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+# Book 테이블
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    authors = models.ManyToManyField('Author') # M:N 관계
+    publisher = models.ForeignKey('Publisher', on_delete=models.CASCADE)
+    publication_date = models.DateField()
+    # models.ManyToManyField('참조테이블')
+    # - 다대다(M:N)의 관계로 필드를 설정
+
+    # models.ForeignKey('참조테이블', 옵션)
+    # - Book(N) : Publisher(1) 관계로 필드를 설정
+    
+
+# Author 테이블
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+    salutation = models.CharField(max_length=100)   # 인사말
+    email = models.EmailField()
+
+
+# Publisher 테이블
+class Publisher(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=100)
+    website = models.URLField()
