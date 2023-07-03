6b7e481867612b5397502e2d05cfcbc527057e0e
seeyoo <lukdown10@gmail.com>
2023-07-03 12:25:12 +0900

    ,.,.

--- code/myform/myapp/forms.py
+++ code/myform/myapp/forms.py
@@ -1,8 +1,12 @@
 from django import forms
 
 # Form 양식 만들기
+# froms.Form 클래스를 상속받아, Form 양식 클래스 정의
 class NameForm(forms.Form):
     your_name = forms.CharField(label="이름", max_length=100)
+    your_age = forms.IntegerField(label="나이", max_value=100)
+    your_info = forms.CharField(label="자기소개", max_length=200)
+    
     
     # 화면에 렌더링 되는 코드
     '''
