a2b88537dc10c5de25f7ae6d548e45f13c4d278a
seeyoo <lukdown10@gmail.com>
2023-07-03 12:25:26 +0900

    ,.,.

--- code/myform/myapp/urls.py
+++ code/myform/myapp/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'myapp'
 urlpatterns = [
     path("", views.index, name="index"),
+    path("results", views.results, name="results"),
     
 ]
 
--- code/myform/myapp/views.py
+++ code/myform/myapp/views.py
@@ -13,9 +13,8 @@ def index(request):
         # 유효성 검사 
         if form.is_valid():
             # 유효성 검사 로직
-            pass
             # 결과화면으로 이동
-            return HttpResponseRedirect('/results')
+            return HttpResponseRedirect('/myapp/results')
     
     # GET
     else :
@@ -24,3 +23,10 @@ def index(request):
     context = {'form' : form}
     
     return render(request, 'myapp/index.html', context)
+
+
+
+# 결과 화면
+def results(request):
+    
+    return render(request, 'myapp/results.html' )
