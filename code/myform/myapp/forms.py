05b58ae12683081b2b4f2d4de460a8ee20434f54
seeyoo <lukdown10@gmail.com>
2023-07-03 12:02:43 +0900

    ,.,.

--- code/myform/myapp/forms.py
+++ code/myform/myapp/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 # Form 양식 만들기
 class NameForm(forms.Form):
-    your_name = forms.CharField(label="이름 : ", max_length=100)
+    your_name = forms.CharField(label="이름", max_length=100)
     
     # 화면에 렌더링 되는 코드
     '''
--- code/myform/myapp/views.py
+++ code/myform/myapp/views.py
@@ -1,3 +1,26 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from .forms import NameForm
 
 # Create your views here.
+def index(request):
+    
+    # POST
+    if request.method == 'POST':
+        
+        form = NameForm(request.POST)   # 요청 데이터 포함한 객체
+        
+        # 유효성 검사 
+        if form.is_valid():
+            # 유효성 검사 로직
+            pass
+            # 결과화면으로 이동
+            return HttpResponseRedirect('/results')
+    
+    # GET
+    else :
+        form = NameForm()       # NameForm 객체 생성
+    
+    context = {'form' : form}
+    
+    return render(request, 'myapp/index.html', context)
