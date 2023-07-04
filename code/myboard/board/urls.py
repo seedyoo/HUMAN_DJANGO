55b2a69e7e1523b3b86e75c5e788d0891661c135
seeyoo <lukdown10@gmail.com>
2023-07-04 14:52:53 +0900

    .,.,.,.,

--- code/myboard/board/urls.py
+++ code/myboard/board/urls.py
@@ -6,9 +6,20 @@ from . import views
 app_name = 'board'
 urlpatterns = [
     # 게시글
-    # '/board/'
+    # 게시글 목록 - '/board/'
     path('', views.BoardList.as_view(), name='board_list'),
     
-    # '/board/board/10'
-    path('/<int:pk>', views.BoardDetail.as_view(), name='board_detail'),
+    # 게시글 조회 - '/board/board/10'
+    path('<int:pk>', views.BoardDetail.as_view(), name='board_detail'),
+
+    # 게시글 쓰기 - '/board/insert'
+    path('insert/', views.BoardInsert.as_view(), name='board_insert'),
+    
+    # 게시글 수정 - '/board/update'
+    path('update/<int:pk>', views.BoardUpdate.as_view(), name='board_update'),
+    
+    
+    
+    
+    
 ]
--- code/myboard/board/views.py
+++ code/myboard/board/views.py
@@ -1,10 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views.generic import ListView, DetailView
+from django.views.generic.base import TemplateView
 from .models import *
 
 # Create your views here.
+# 게시글 목록
 class BoardList(ListView):
     model = Board
     
+    
+# 게시글 상세
 class BoardDetail(DetailView):
     model = Board
+
+
+# 게시글 쓰기
+class BoardInsert(TemplateView):
+    template_name = 'board/board_insert.html'
+    
+    def post(self, request):
+        # 요청 메시지의 title, content 가져오기
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+
+        # Board 모델에 데이터 저장
+        board = Board(title=title, content=content)
+        board.save()
+
+        # 게시글 목록으로 이동
+        return redirect('/board')
+    
+
+
+# 게시글 수정
+class BoardUpdate(TemplateView):
+    template_name = 'board/board_update.html'
+    
+    # context
+    
+    # post
+
+
+
+
+
+
